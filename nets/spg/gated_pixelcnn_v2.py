f72c19cab4657465afebbc3ba0b4060570687c19
cherrynniii <chaelin20760@gmail.com>
2025-08-02 16:42:24 +0900

    d

--- nets/spg/gated_pixelcnn_v2.py
+++ nets/spg/gated_pixelcnn_v2.py
@@ -73,40 +73,40 @@ class GatedMaskedConv2d(nn.Module):
         if self.mask_type == 'A':
             self.make_causal()
 
-        print("h: ", h.shape)
+        print("b h: ", h.shape)
         h = self.class_cond_embedding(h.to(self.class_cond_embedding.weight.device))    # 클래스 조건 임베딩
-        print("h: ", h.shape)
+        print("b h: ", h.shape)
         h_vert = self.vert_stack(x_v)   # vertical 처리
-        print("h_vert: ", h_vert.shape)
+        print("b h_vert: ", h_vert.shape)
         h_vert = h_vert[:, :, :x_v.size(-2), :]     # 크기 맞춰줌
-        print("h_vert: ", h_vert.shape)
+        print("b h_vert: ", h_vert.shape)
         out_v = self.gate(h_vert + h[:, :, None, None])     # 조건 정보 더하고 게이팅
-        print("out_v: ", out_v.shape)
+        print("b out_v: ", out_v.shape)
 
         # horizontal 처리
         if self.bh_model:
             h_horiz = self.horiz_stack(x_h)
-            print("h_vert: ", h_vert.shape)
+            print("b h_vert: ", h_vert.shape)
             h_horiz = h_horiz[:, :, :, :x_h.size(-1)]
-            print("h_vert: ", h_vert.shape)
+            print("b h_vert: ", h_vert.shape)
             v2h = self.vert_to_horiz(h_vert)    # vertical 정보를 반영해 전달
-            print("v2h: ", v2h.shape)
+            print("b v2h: ", v2h.shape)
 
             out = self.gate(v2h + h_horiz + h[:, :, None, None])
-            print("out: ", out.shape)
+            print("b out: ", out.shape)
             if self.residual:
                 out_h = self.horiz_resid(out) + x_h
-                print("out_h: ", out_h.shape)
+                print("b out_h: ", out_h.shape)
             else:
                 out_h = self.horiz_resid(out)
-                print("out_h: ", out_h.shape)
+                print("b out_h: ", out_h.shape)
         else:
             if self.residual:
                 out_v = self.horiz_resid(out_v) + x_v
-                print("out_v: ", out_v.shape)
+                print("b out_v: ", out_v.shape)
             else:
                 out_v = self.horiz_resid(out_v)
-                print("out_v: ", out_v.shape)
+                print("b out_v: ", out_v.shape)
             out_h = out_v
 
         return out_v, out_h
@@ -168,7 +168,7 @@ class GatedPixelCNN(nn.Module):
         shp = x.size() + (-1,)
         x = self.embedding(x.view(-1)).view(shp)  # (B, H, W, C)
         print("x: ", x.shape)
-        x = x.permute(0, 3, 1, 2)  # (B, C, W, W)
+        x = x.permute(0, 3, 1, 2)  # (B, C, H, W)
         print("x: ", x.shape)
 
         x_v, x_h = (x, x)
--- train_body_pixel.sh
+++ train_body_pixel.sh
@@ -1,6 +1,6 @@
 python -W ignore scripts/train.py \
 --save_dir experiments \
---exp_name original_test \
+--exp_name original_body \
 --use_wandb \
---speakers oliver \
+--speakers oliver seth conan chemistry \
 --config_file ./config/body_pixel.json
